92ebed59ae311f7f4cf7bcebcbe22808f5e99885
notaryzw3b <modz.ayrton@gmail.com>
2021-05-22 20:58:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3010;
+const path = require('path');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
