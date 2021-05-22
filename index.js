7c34690c688b4465ac32eed24045a0ae077ce826
notaryzw3b <modz.ayrton@gmail.com>
2021-05-22 21:01:21 +0200

    express template for stackblitz

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3010;
 const path = require('path');
 
+app.use(express.static('static'));
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve('pages/index.html'));
 });
